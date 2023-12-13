65900d23cef961007f1ce3de73ad9a7c40d7ef3a
renanalencardev <renanalencar02@gmail.com>
2023-12-13 14:23:45 -0300

    iniciando projeto

---
+++ src/main/java/usa/ggti/desafio08/Desafio08Application.java
@@ -0,0 +1,13 @@
+package usa.ggti.desafio08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio08Application.class, args);
+	}
+
+}
