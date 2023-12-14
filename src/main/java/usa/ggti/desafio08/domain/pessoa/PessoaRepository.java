3826e2132539148158d30c8a9ec0eb8c1b41ddd1
renanalencardev <renanalencar02@gmail.com>
2023-12-14 10:58:29 -0300

    Criado o repositório de pessoa

---
+++ src/main/java/usa/ggti/desafio08/domain/pessoa/PessoaRepository.java
@@ -0,0 +1,6 @@
+package usa.ggti.desafio08.domain.pessoa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+}
