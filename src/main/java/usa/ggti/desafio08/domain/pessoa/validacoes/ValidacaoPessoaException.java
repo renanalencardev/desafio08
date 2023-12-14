2492eed714cc99991ce7fadef677d75ca44a7e60
renanalencardev <renanalencar02@gmail.com>
2023-12-14 14:28:46 -0300

    Criada a exceção de validação de pessoa

---
+++ src/main/java/usa/ggti/desafio08/domain/pessoa/validacoes/ValidacaoPessoaException.java
@@ -0,0 +1,7 @@
+package usa.ggti.desafio08.domain.pessoa.validacoes;
+
+public class ValidacaoPessoaException extends RuntimeException {
+    public ValidacaoPessoaException(String mensagem){
+        super(mensagem);
+    }
+}
