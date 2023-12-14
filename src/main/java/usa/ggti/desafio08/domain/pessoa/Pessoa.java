6618aa2990a287bcd27bc152001ebb210b012d74
renanalencardev <renanalencar02@gmail.com>
2023-12-14 10:58:09 -0300

    Criada a entidade pessoa

---
+++ src/main/java/usa/ggti/desafio08/domain/pessoa/Pessoa.java
@@ -0,0 +1,28 @@
+package usa.ggti.desafio08.domain.pessoa;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "Pessoa")
+@Table(name = "pessoas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String idade;
+
+    public Pessoa(DadosCadastroPessoaDto dados) {
+        this.nome = dados.nome();
+        this.email = dados.email();
+        this.idade = dados.idade();
+    }
+}
