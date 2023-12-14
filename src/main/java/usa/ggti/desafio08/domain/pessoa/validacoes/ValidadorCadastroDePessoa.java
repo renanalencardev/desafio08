52744b43e40e4556ae93f4bd7bcb17dd46d6d2bf
renanalencardev <renanalencar02@gmail.com>
2023-12-14 14:28:18 -0300

    Criada interface de validação de pessoa

---
+++ src/main/java/usa/ggti/desafio08/domain/pessoa/validacoes/ValidadorCadastroDePessoa.java
@@ -0,0 +1,7 @@
+package usa.ggti.desafio08.domain.pessoa.validacoes;
+
+import usa.ggti.desafio08.domain.pessoa.DadosCadastroPessoaDto;
+
+public interface ValidadorCadastroDePessoa {
+    void validar(DadosCadastroPessoaDto daos);
+}
