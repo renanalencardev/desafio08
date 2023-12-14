f81c4f66c6a4a05ba9676321c277e55a0ab87209
renanalencardev <renanalencar02@gmail.com>
2023-12-14 15:45:40 -0300

    Inclusão das validações no serviço de cadastro de pessoa

--- src/main/java/usa/ggti/desafio08/domain/pessoa/ServicoCadastroDePessoa.java
+++ src/main/java/usa/ggti/desafio08/domain/pessoa/ServicoCadastroDePessoa.java
@@ -2,6 +2,7 @@ package usa.ggti.desafio08.domain.pessoa;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import usa.ggti.desafio08.domain.pessoa.validacoes.ValidadorCadastroDePessoa;
 
 import java.util.List;
 
@@ -9,8 +10,11 @@ import java.util.List;
 public class ServicoCadastroDePessoa {
     @Autowired
     private PessoaRepository pessoaRepository;
+    @Autowired
+    private List<ValidadorCadastroDePessoa> validacoes;
 
     public DadosDetalhamentoPessoaDto cadastrar(DadosCadastroPessoaDto dados){
+        validacoes.forEach(v -> v.validar(dados));
         var pessoa = new Pessoa(dados);
         pessoaRepository.save(pessoa);
         return new DadosDetalhamentoPessoaDto(pessoa);
