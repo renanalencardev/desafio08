a9b670f7bd6dbd22c469a62068bfa5d19929a53b
renanalencardev <renanalencar02@gmail.com>
2023-12-15 16:38:17 -0300

    Tratamento de erros

--- src/main/java/usa/ggti/desafio08/infra/exception/TratadorDeErros.java
+++ src/main/java/usa/ggti/desafio08/infra/exception/TratadorDeErros.java
@@ -13,6 +13,9 @@ import usa.ggti.desafio08.model.pessoa.validacoes.ValidacaoPessoaException;
 public class TratadorDeErros {
     @ExceptionHandler(ValidacaoPessoaException.class)
     public ResponseEntity tratarErroCadastroDePessoa(ValidacaoPessoaException exception){
+        var erro = exception.getMessage();
+        var status = HttpStatus.BAD_REQUEST.toString();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DadosErrosValidacaoPessoa(status, erro));
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity tratarErroAtributoNull(MethodArgumentNotValidException exception){
@@ -26,7 +29,12 @@ public class TratadorDeErros {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DadosErrosValidacaoPessoa(status, erro).toString());
     }
 
+    private record DadosErrosValidacaoPessoa(String status, String message){
+        public DadosErrosValidacaoPessoa(String status, String message){
             this.message = message;
+            this.status = status;
+        }
+    }
     private record DadosErrosArgumentoInvalido(String field, String message){
         public DadosErrosArgumentoInvalido(FieldError erro){
             this(erro.getField(), erro.getDefaultMessage());
