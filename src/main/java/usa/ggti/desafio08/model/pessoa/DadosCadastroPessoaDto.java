ebd54b31aa47074b97b0a7138c23166c5512cec7
renanalencardev <renanalencar02@gmail.com>
2023-12-15 16:35:19 -0300

    Inclusão de tratamento de nulos no dto de cadastro de pessoa

--- src/main/java/usa/ggti/desafio08/model/pessoa/DadosCadastroPessoaDto.java
+++ src/main/java/usa/ggti/desafio08/model/pessoa/DadosCadastroPessoaDto.java
@@ -1,7 +1,13 @@
 package usa.ggti.desafio08.model.pessoa;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record DadosCadastroPessoaDto(String nome, String email, String idade) {
+public record DadosCadastroPessoaDto(
+        @NotNull(message = "O campo nome não pode ser nulo")
+        String nome,
+        @NotNull(message = "O campo email não pode ser nulo")
+        String email,
+        @NotNull(message = "O campo idade não pode ser nulo")
+        String idade) {
+
 }
