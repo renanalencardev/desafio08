a6024cd2b5759e2c2e9b62d09dc5ebf96d6433e6
renanalencardev <renanalencar02@gmail.com>
2023-12-15 16:37:07 -0300

    Alterado retorno da exceção de validação de pessoa usando dto

--- src/main/java/usa/ggti/desafio08/infra/exception/TratadorDeErros.java
+++ src/main/java/usa/ggti/desafio08/infra/exception/TratadorDeErros.java
@@ -2,6 +2,9 @@ package usa.ggti.desafio08.infra.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import usa.ggti.desafio08.model.pessoa.validacoes.ValidacaoPessoaException;
@@ -10,12 +13,23 @@ import usa.ggti.desafio08.model.pessoa.validacoes.ValidacaoPessoaException;
 public class TratadorDeErros {
     @ExceptionHandler(ValidacaoPessoaException.class)
     public ResponseEntity tratarErroCadastroDePessoa(ValidacaoPessoaException exception){
-        var erros = exception.getMessage();
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DadosErrosValidacao(erros));
     }
-    private record DadosErrosValidacao(String message){
-        public DadosErrosValidacao(String message){
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarErroAtributoNull(MethodArgumentNotValidException exception){
+        var erros = exception.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosArgumentoInvalido::new).toList());
+    }
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> tratarCorpoDaRequisicaoAusente(HttpMessageNotReadableException exception) {
+        var erro = "Corpo da requisição ausente ou inválido.";
+        var status = HttpStatus.BAD_REQUEST.toString();
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new DadosErrosValidacaoPessoa(status, erro).toString());
+    }
+
             this.message = message;
+    private record DadosErrosArgumentoInvalido(String field, String message){
+        public DadosErrosArgumentoInvalido(FieldError erro){
+            this(erro.getField(), erro.getDefaultMessage());
         }
     }
 }
