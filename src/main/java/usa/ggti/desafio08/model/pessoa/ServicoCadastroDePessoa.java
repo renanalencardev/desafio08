7d522e1dc607de371bae2ec68f00f4ca226f01ff
renanalencardev <renanalencar02@gmail.com>
2023-12-15 16:34:36 -0300

    Incluído tratamento de nome no serviço de cadastro de pessoa

--- src/main/java/usa/ggti/desafio08/model/pessoa/ServicoCadastroDePessoa.java
+++ src/main/java/usa/ggti/desafio08/model/pessoa/ServicoCadastroDePessoa.java
@@ -3,6 +3,7 @@ package usa.ggti.desafio08.model.pessoa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import usa.ggti.desafio08.model.pessoa.validacoes.ValidadorCadastroDePessoa;
+import usa.ggti.desafio08.util.PadronizarNome;
 
 import java.util.List;
 
@@ -15,7 +16,11 @@ public class ServicoCadastroDePessoa {
 
     public DadosDetalhamentoPessoaDto cadastrar(DadosCadastroPessoaDto dados){
         validacoes.forEach(v -> v.validar(dados));
-        var pessoa = new Pessoa(dados);
+
+        PadronizarNome padronizarNome = new PadronizarNome();
+        String nome = padronizarNome.primeiraLetraMaiuscula(dados.nome());
+
+        var pessoa = new Pessoa(new DadosCadastroPessoaDto(nome, dados.email(), dados.idade()));
         pessoaRepository.save(pessoa);
         return new DadosDetalhamentoPessoaDto(pessoa);
     }
